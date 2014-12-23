1cbe0b13d97a8ef41961112d3ed79be900aa6751
Scott Bedard <scottbedard90@gmail.com>
2014-12-23 17:03:40 -0500

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,26 @@
+<?php namespace Bedard\Shop;
+
+use System\Classes\PluginBase;
+
+/**
+ * Shop Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Shop',
+            'description' => 'No description provided yet...',
+            'author'      => 'Bedard',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+}
