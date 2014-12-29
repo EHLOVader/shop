3bb08f8b7ac86c684204449d716d15279c7ff79c
Scott Bedard <scottbedard90@gmail.com>
2014-12-28 23:56:16 -0500

    Codes list display

--- controllers/Codes.php
+++ controllers/Codes.php
@@ -2,6 +2,7 @@
 
 use BackendMenu;
 use Backend\Classes\Controller;
+use Bedard\Shop\Models\PaySettings;
 
 /**
  * Codes Back-end Controller
@@ -16,10 +17,31 @@ class Codes extends Controller
     public $formConfig = 'config_form.yaml';
     public $listConfig = 'config_list.yaml';
 
+    /**
+     * Codes Constructor
+     */
     public function __construct()
     {
         parent::__construct();
-
         BackendMenu::setContext('Bedard.Shop', 'shop', 'codes');
+
+        $this->addCss('/plugins/bedard/shop/assets/css/backend.css');
+    }
+
+    /**
+     * Codes Index
+     */
+    public function index()
+    {
+        $this->vars['currency'] = PaySettings::get('currency');
+        $this->asExtension('ListController')->index();
+    }
+
+    /**
+     * Extend the list query to eager load carts
+     */
+    public function listExtendQuery($query, $definition = null)
+    {
+        //$query->with('carts');
     }
 }
--- models/Code.php
+++ models/Code.php
@@ -48,4 +48,83 @@ class Code extends Model
     public $customMessages = [
         'code.regex' => 'Codes may only contain alpha-numeric characters, spaces, hyphens, and underscores.'
     ];
+
+    /**
+     * Add the dynamic validation rules
+     */
+    public function beforeValidate()
+    {
+        // Only check the start / end dates when both are provided
+        if ($this->start_date && $this->end_date)
+            $this->rules['end_date'] .= '|after:start_date';
+
+        // If this is a percentage discount, add integer and max validation
+        if ($this->is_percentage)
+            $this->rules['amount'] .= '|integer|max:100';
+    }
+
+    /**
+     * Status Attributes
+     */
+    public function getIsUpcomingAttribute()
+    {
+        // Returns true for upcoming codes
+        return $this->start_date > date('Y-m-d H:i:s');
+    }
+    
+    public function getIsRunningAttribute()
+    {
+        // Returns true for running codes
+        $now = date('Y-m-d H:i:s');
+        return 
+            (!$this->start_date || $this->start_date <= $now) && (!$this->end_date || $this->end_date >= $now) &&
+            ($this->limit == 0 || $this->uses < $this->limit);
+    }
+
+    public function getIsCompleteAttribute()
+    {
+        // Returns true for completed codes
+        return 
+            ($this->end_date && $this->end_date < date('Y-m-d H:i:s')) ||
+            ($this->limit > 0 && $this->uses >= $this->limit);
+    }
+
+    /**
+     * Floor percentage discounts, and round exact amount discounts
+     * @param   string  $amount
+     */
+    public function setAmountAttribute($amount)
+    {
+        if (!isset($this->attributes['is_percentage']))
+            $this->attributes['amount'] = $amount;
+
+        else
+            $this->attributes['amount'] = $this->attributes['is_percentage']
+                ? $this->attributes['amount'] = floor($amount)
+                : round($this->attributes['amount'], 2);
+    }
+
+    /**
+     * Floor percentage discounts
+     * @return  string (numeric)
+     */
+    public function getAmountAttribute()
+    {
+        if (!isset($this->attributes['amount']))
+            return;
+
+        return $this->is_percentage
+            ? floor($this->attributes['amount'])
+            : $this->attributes['amount'];
+    }
+
+    /**
+     * Returns the number of times the promo code has been used
+     * @return  integer
+     */
+    public function getUsesAttribute()
+    {
+        return 0;
+    }
+
 }
