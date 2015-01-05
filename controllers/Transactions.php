8e85225410478fb97808ac0952163fd8f12808c7
Scott Bedard <scottbedard90@gmail.com>
2015-01-05 09:12:30 -0500

    Eager loading

--- controllers/Transactions.php
+++ controllers/Transactions.php
@@ -16,10 +16,32 @@ class Transactions extends Controller
     public $formConfig = 'config_form.yaml';
     public $listConfig = 'config_list.yaml';
 
+    /**
+     * Transactions Constructor
+     */
     public function __construct()
     {
         parent::__construct();
 
         BackendMenu::setContext('Bedard.Shop', 'shop', 'transactions');
+    }
+
+    /**
+     * Transactions Index
+     */
+    public function index()
+    {
+        $this->asExtension('ListController')->index();
+    }
+
+    /**
+     * Extend the list query to eager load customers, cart items, and coupons
+     */
+    public function listExtendQuery($query, $definition = null)
+    {
+        $query->with('customer')
+            ->with('cart.items')
+            ->with('cart.coupon')
+            ->isComplete();
     }
 }
