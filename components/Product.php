4b293e9ac1799eff6fd2c86a2e60d7c0cedf0878
Scott Bedard <scottbedard90@gmail.com>
2015-01-09 20:53:57 -0500

    Remove ounces

--- components/Product.php
+++ components/Product.php
@@ -23,7 +23,6 @@ class Product extends ComponentBase
     public $description;    // string
     public $price;          // float
     public $fullPrice;      // float
-    public $ounces;         // integer
     public $isVisible;      // boolean
     public $isDiscounted;   // boolean
     public $inStock;        // boolean
